b222079673a7a07a500340f1058ef4a8e8a75c83
phpcrazy <phpcrazy@gmail.com>
2020-02-08 18:27:06 +0630

    DatabasePDF

--- phpstarter/app/controller/controllers.php
+++ phpstarter/app/controller/controllers.php
@@ -1,22 +1,46 @@
 <?php
 
 function _indexController() {
-    load_view("indexx");
+    load_view("index");
 }
 
-function _blogController($value = null) {
-    if($value == "foo") {
-        $data = [
-            'title' => 'Myanmar Links',
-            "another" => 'Foo'
-        ];
-    } else {
-        $data = [
-            'title' => config_get("app.app_name"),
-            "another" => config_get("database.dbname")
-        ];
+function _studentController() {
+    $data = [
+        "students" => [
+            [
+                "id" => 1,
+                "name"  => 'Aung Aung',
+                "address" => 'Hledan'
+            ],
+            [
+                "id" => 1,
+                "name"  => 'Bung Bung',
+                "address" => 'Hledan'
+            ],
+            [
+                "id" => 1,
+                "name"  => 'Cung Cung',
+                "address" => 'Hledan'
+            ]
+        ]
+    ];
+    load_view("student", $data);
     }
-    
-   
-    load_view("blog", $data);
-}
+        
+        function _blogController($value = null) {
+            if($value == "foo") {
+                $data = [
+                    'title' => 'Myanmar Links',
+                    "another" => 'Foo'
+                ];
+            } else {
+                $data = [
+                    'title' => config_get("app.app_name"),
+                    "another" => config_get("database.dbname")
+                ];
+            }
+            
+            
+            load_view("blog", $data);
+        }
+        
---
+++ phpstarter/app/view/student.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student List</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       <?php foreach($students as $student) { ?>
+                            <tr>
+                                <td><?= $student['id'] ?></td>
+                                <td><?= $student['name'] ?></td>
+                                <td><?= $student['address'] ?></td>
+                            </tr>
+                       <?php } ?>
+                    </tbody>
+                </table>
+            
+            </div>
+        </div>
+    </div>
+</body>
+</html>
